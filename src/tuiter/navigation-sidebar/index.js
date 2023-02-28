d7609819b4fb95e996a6de5a681b60e7cdf95399
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-02-27 19:17:09 -0800

    a6 tuiter

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,41 +7,53 @@ const NavigationSidebar = () => {
     const paths = pathname.split('/')
     const active = paths[2];
     return (
-        <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
+        <div className="list-group"
+             style={{minWidth: 'fit-content'}}>
+            <a className="list-group-item">
+                <i className="bi bi-twitter"></i>
+            </a>
             <Link to="/tuiter/home" className={`list-group-item ${active === 'home'?'active':''}`}>
-                Home
+                <i className="bi bi-house-door-fill me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Home</span>
             </Link>
             <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore' || active === '' || active === undefined?'active':''}`}>
-                Explore
+                <i className="bi bi-hash me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Explore</span>
             </Link>
             <Link to="/" className="list-group-item">
-                Labs
+                <i className="bi bi-person-workspace me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Labs</span>
             </Link>
 
             <a className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-                Notifications
+                <i className="bi bi-bell-fill me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Notifications</span>
             </a>
             <a className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-                Messages
+                <i className="bi bi-envelope-fill me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Messages</span>
             </a>
             <a className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-                Bookmarks
+                <i className="bi bi-bookmark-fill me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Bookmarks</span>
             </a>
             <a className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-                Lists
+                <i className="bi bi-list-ul me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Lists</span>
             </a>
             <a className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-                Profile
+                <i className="bi bi-person-fill me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">Profile</span>
             </a>
             <a className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-                More
+                <i className="bi bi-three-dots me-2"></i>
+                <span className="wd-nav-bar-text d-none d-xl-inline">More</span>
             </a>
         </div>
     );
