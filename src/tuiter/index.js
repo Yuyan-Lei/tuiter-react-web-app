7ec62b95c539ee6af363c76c925d7ce5f6850177
Yuyan-Lei <82356933+Yuyan-Lei@users.noreply.github.com>
2023-03-04 11:00:23 -0800

    Update index.js

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -21,7 +21,7 @@ function Tuiter() {
                     <Route path="*" element={<ExploreComponent/>} />
                 </Routes>
             </div>
-            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <div className="d-none d-lg-block col-lg-4 col-xl-4">
                 <WhoToFollowList />
             </div>
         </div>
