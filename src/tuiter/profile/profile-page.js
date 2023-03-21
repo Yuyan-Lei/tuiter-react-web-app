0396e172e8c5e73d2eeb74d8ea9641b736d00097
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-03-21 15:38:03 -0700

    a7 Tuiter Challenge 1

--- src/tuiter/profile/profile-page.js
+++ src/tuiter/profile/profile-page.js
@@ -21,45 +21,59 @@ const ProfilePage = () => {
             {/*banner*/}
             <div>
                 <img src={profile.bannerPicture}
-                     width="200px"
-                     height="400px"/>
+                     width="100%"
+                     height="200px"/>
             </div>
 
             {/*profile picture*/}
             <div className="row">
-                <div className="float-start">
+                <div className="col-8">
                     <img src={profile.profilePicture}
-                            width="100px" height="100px"/>
+                            width="100px" height="100px"
+                         style={{position: 'relative', top: "-50%"}}
+                    className={"ms-4 rounded"}/>
                 </div>
-                <div className="float-end">
-                    <button className="btn btn-primary">Edit Profile</button>
+                <div className="col-4 mt-2">
+                    <a href="#"
+                       className="btn btn-outline-dark ms-5"
+                       style={{borderRadius: "18px", borderColor:"lightgrey", fontWeight:"bold"}}
+                       role="button">
+                        Edit Profile
+                    </a>
                 </div>
             </div>
 
             {/*name*/}
             <div>
-                <h1>{profile.firstName} {profile.lastName}</h1>
-                <p>{profile.handle}</p>
-                <br/>
+                <span style={{fontWeight:"bold", fontSize:22}}>{profile.firstName} {profile.lastName}</span>
+                <p style={{color: 'gray'}}>{profile.handle}</p>
             </div>
 
             {/*bio*/}
-            <div>
-                <p>{profile.bio}</p>
-                <br/>
+            <div className="mb-2">
+                {profile.bio}
             </div>
 
             {/*location, birthday, dateJoined*/}
-            <div>
+            <div className="mb-2" style={{color: 'gray'}}>
+                <i className="bi bi-geo-alt me-1"></i>
                 <span>{profile.location} </span>
+                <i className="bi bi-lightbulb ms-2 me-1"></i>
                 <span>Born {profile.dateOfBirth} </span>
+                <i className="bi bi-calendar3 ms-2 me-1"></i>
                 <span>Joined {profile.dateJoined}</span>
             </div>
 
             {/*following, followers*/}
             <div>
-                <span>{profile.followingCount} Following </span>
-                <span>{profile.followersCount} Followers</span>
+                <span>
+                    <span style={{fontWeight:"bold"}}>{profile.followingCount}</span>
+                    <span className="ms-1 me-4" style={{color: 'grey'}}>Following</span>
+                </span>
+                <span>
+                    <span style={{fontWeight:"bold"}}>{profile.followersCount}</span>
+                    <span className="ms-1 me-4" style={{color: 'grey'}}>Followers</span>
+                </span>
             </div>
 
         </div>
