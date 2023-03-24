ca68efa5fa14e20ce5651298fef26063f9b7db28
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-03-24 15:34:22 -0700

    a8

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+// const API_BASE = process.env.REACT_APP_API_BASE;
+// const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = 'https://tuiter-node-server-app-qpfi.onrender.com/api/tuits'
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
 }
 
-export const findTuits = async (tuit) => {
+export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
     const tuits = response.data;
     return tuits;
