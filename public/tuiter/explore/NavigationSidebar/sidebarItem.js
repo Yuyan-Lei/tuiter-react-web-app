b593dc789a8eca2702f951845f12ebd0cdf01571
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-03-29 21:38:48 -0700

    a8

--- public/tuiter/explore/NavigationSidebar/sidebarItem.js
+++
@@ -1,9 +0,0 @@
-const sidebarItem = (item, active) => {
-    return(`
-     <a class="list-group-item list-group-item-action ${active === item.name ? 'active' : ''}" href="${item.hyperlink}">
-       <i class="${item.icon} fa-fw"></i>
-       <span class="wd-nav-bar-text d-none d-xl-inline">${item.name}</span>
-     </a>
- `);
-}
-export default sidebarItem;
